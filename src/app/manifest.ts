2ed4f9fac27bff169dc31b7e2c1aff1528e625a8
Spike <kikyouning@outlook.com>
2025-03-05 00:17:33 +0000

    feat: update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,21 +1,21 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Meow',
     short_name: 'Meow',
     description: 'Meow meow meow',
-    start_url: '/',
+    start_url: '/meow/bill',
     display: 'standalone',
-    "icons": [
+    icons: [
       {
-        "src": "/web-app-manifest-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "maskable"
-      }
+        src: '/poker/red_joker.svg',
+        sizes: '192x192',
+        type: 'image/svg',
+        purpose: 'maskable',
+      },
     ],
-    "theme_color": "#ffffff",
-    "background_color": "#003345",
-  }
+    theme_color: '#ffffff',
+    background_color: '#003345',
+  };
 }
