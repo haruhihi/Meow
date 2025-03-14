67c30963c8a786d1aa8adf6250b07615a1971a24
Spike <kikyouning@outlook.com>
2025-03-14 08:45:32 +0000

    feat: pagination, menu

--- src/app/ai-hub/home/side-bar/index.tsx
+++ src/app/ai-hub/home/side-bar/index.tsx
@@ -1,12 +1,76 @@
 import Tool from '@static/tool.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
 import { UpOutlined } from '@ant-design/icons';
 import { Icon } from '../icon';
 import { AI_Tool } from '@prisma/client';
+import { items } from './config';
+import { Menu, MenuProps } from 'antd';
 
-export const SideBar: React.FC<{ tools: AI_Tool[] }> = (props) => {
-  const { tools } = props;
+interface LevelKeysProps {
+  key?: string;
+  children?: LevelKeysProps[];
+}
+
+export const SideBar: React.FC<{
+  tools: AI_Tool[];
+  onClickItem: (v: string[]) => void;
+}> = (props) => {
+  const { tools, onClickItem } = props;
+  const [stateOpenKeys, setStateOpenKeys] = useState<string[]>(['Chatbot', 'Text']);
+
+  const getLevelKeys = (items1: LevelKeysProps[]) => {
+    const key: Record<string, number> = {};
+    const func = (items2: LevelKeysProps[], level = 1) => {
+      items2.forEach((item) => {
+        if (item.key) {
+          key[item.key] = level;
+        }
+        if (item.children) {
+          func(item.children, level + 1);
+        }
+      });
+    };
+    func(items1);
+    return key;
+  };
+
+  const levelKeys = getLevelKeys(items as LevelKeysProps[]);
+
+  const onOpenChange: MenuProps['onOpenChange'] = (openKeys) => {
+    console.log('222');
+    const currentOpenKey = openKeys.find((key) => stateOpenKeys.indexOf(key) === -1);
+    // open
+    if (currentOpenKey !== undefined) {
+      const repeatIndex = openKeys
+        .filter((key) => key !== currentOpenKey)
+        .findIndex((key) => levelKeys[key] === levelKeys[currentOpenKey]);
+
+      setStateOpenKeys(
+        openKeys
+          // remove repeat key
+          .filter((_, index) => index !== repeatIndex)
+          // remove current level all child
+          .filter((key) => levelKeys[key] <= levelKeys[currentOpenKey])
+      );
+    } else {
+      // close
+      setStateOpenKeys(openKeys);
+    }
+  };
+  return (
+    <Menu
+      mode="inline"
+      defaultSelectedKeys={['All Tools']}
+      openKeys={stateOpenKeys}
+      onOpenChange={onOpenChange}
+      style={{ width: 256 }}
+      items={items}
+      onClick={(item) => {
+        onClickItem(item.keyPath);
+      }}
+    />
+  );
   return (
     <div className={styles.sideBarWrap}>
       <ListItem icon={<Tool width="16px" height="16px" />} text="All Tools" appendix={tools.length} />
@@ -14,8 +78,8 @@ export const SideBar: React.FC<{ tools: AI_Tool[] }> = (props) => {
         Popular Tools <UpOutlined width={16} height={16} />
       </div>
       <div>
-        {tools.map((tool, index) => {
-          return <ListItem key={index} icon={tool.icon} text={tool.name} link={tool.link} tool={tool} />;
+        {CategoryConfig.map((category, index) => {
+          return <ListItem key={index} icon={category.icon} text={category.label} />;
         })}
       </div>
     </div>
